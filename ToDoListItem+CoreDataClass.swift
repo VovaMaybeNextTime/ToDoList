cc33bc5e93ee9fec22c5ed7431bac8896c377055
VovaMaybeNextTime <48494728+VovaMaybeNextTime@users.noreply.github.com>
2023-03-29 13:32:36 +0300

    Add files via upload

---
+++ ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoList
+//
+//  Created by Владимир Жнакин on 29.03.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
